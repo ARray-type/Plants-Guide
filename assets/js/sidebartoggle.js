1359f27d493fcce43a60d6414af31baa9bbffb09
ARray-type <136046145+ARray-type@users.noreply.github.com>
2025-11-23 13:14:04 +0800

    more changes

--- assets/js/sidebartoggle.js
+++ assets/js/sidebartoggle.js
@@ -1,10 +1,17 @@
 const sidebar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('sidebar-toggle');
+const sidebartoggle = document.getElementById('sidebar-toggle');
+const resources = document.getElementById('resources');
 
-toggleButton.addEventListener('click', toggleEvent)
-    
-function toggleEvent() {
+sidebartoggle.addEventListener('click', sidebartoggled)
+resources.addEventListener('click', resourcestoggled)    
+
+function sidebartoggled() {
     sidebar.classList.toggle('open');
-    toggleButton.classList.toggle('open');
+    sidebartoggle.classList.toggle('open');
     console.log('Sidebar toggled');
+}
+
+function resourcestoggled() {
+    resources.classList.toggle('open')
+    console.log('Resources toggled')
 }
